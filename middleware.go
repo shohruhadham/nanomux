1cff92701ff04286e98803f5bd4f8bc012b68464
Shohruh Adham <shohruh.adham@gmail.com>
2022-01-01 20:53:07 +0500

    The Hr, HrFn, and MwFn converter function have been added.

--- middleware.go
+++ middleware.go
@@ -4,8 +4,10 @@
 package nanomux
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"net/http"
 )
 
 // --------------------------------------------------
@@ -15,7 +17,30 @@ var ErrConflictingMethod = fmt.Errorf("conflicting method")
 
 // --------------------------------------------------
 
-type MiddlewareFunc func(next Handler) Handler
+type MiddlewareFunc func(next Handler) HandlerFunc
+
+// MwFn converts the middleware that takes an http.Handler and returns an
+// http.Handler to the MiddlewareFunc.
+func MwFn(mw func(http.Handler) http.Handler) MiddlewareFunc {
+	return func(next Handler) HandlerFunc {
+		var h http.Handler = http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				next.ServeHTTP(r.Context(), w, r)
+			},
+		)
+
+		h = mw(h)
+
+		return func(
+			c context.Context,
+			w http.ResponseWriter,
+			r *http.Request,
+		) {
+			r = r.WithContext(c)
+			h.ServeHTTP(w, r)
+		}
+	}
+}
 
 // --------------------------------------------------
 
